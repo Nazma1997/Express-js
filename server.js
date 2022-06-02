d520727d3aa542854b548c771426c528e9d319eb
Nazma1997 <nazma.ist.707@gmail.com>
2022-06-02 11:44:48 +0600

    added

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+           res.render('home')
+  
+});
+
+mongoose.connect('mongodb://localhost:27017/test')
+
+   .then(() => {
+      app.listen(5000, () => {
+        console.log('Server is listening on port 8000');
+
+      })
+   })
+   .catch(e => {
+     console.log(e)
+   })
